efb714c68ead758b5375ae2e94872ea33dad7d0b
aloHyomora <alohomoratolit@naver.com>
2025-02-10 17:56:58 +0900

    [Feat] : routing(URL Variable)

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return f"User {username}"
+
 if __name__ == '__main__':
     app.run(debug=True)
