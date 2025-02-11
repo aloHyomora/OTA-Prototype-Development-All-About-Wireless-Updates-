67639f7584077e01c2e6d3711aa79e16ee27eacd
aloHyomora <alohomoratolit@naver.com>
2025-02-11 15:51:45 +0900

    [Feat] : Flask Static File Test2 Redirection

--- appTestStatic.py
+++ appTestStatic.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/img/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('static/img', filename)
 
 # 메인 페이지 라우트
 @app.route('/')
